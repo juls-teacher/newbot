2b29f537737b03084242750471fee289629230c2
juls-teacher <juliayurchyk@gmail.com>
2023-05-06 10:54:17 +0300

    add

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
 import os
+import logging
+
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(
+        f"Message from chat #{update.effective_user.first_name}"
+        f" username {update.effective_chat.id}"
+                )
+    await update.message.reply_text(
+        f"Hello {update.effective_user.first_name} ({update.effective_chat.id})"
+    )
+
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(
+        f"Message from chat #{update.effective_user.first_name}"
+        f" username {update.effective_chat.id}"
+                )
     await update.message.reply_text(
         f"Hello {update.effective_user.first_name} ({update.effective_chat.id})"
     )
@@ -13,5 +33,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(~filters.COMMAND, message))
     app.run_polling()
 
